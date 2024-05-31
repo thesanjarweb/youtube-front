19f1254d4220411ae6dd9f26f0395b2a1aa21c41
thesanjar09 <jumnazarjumnazar614@gmail.com>
2024-05-31 15:31:41 +0300

    Navbar ui

---
+++ script/script.js
@@ -0,0 +1,30 @@
+let ul =document.querySelector('ul');
+ul.style.display ='none'
+
+let isOpened =false;
+
+let menuIcon =document.querySelectorAll('.menu-icon');
+menuIcon[0].addEventListener('click',() => {
+    
+        ul.style.display ='none'
+        isOpened =false
+   
+        document.querySelector("nav").style.left ="0";
+        document.querySelector(".darker").style.display ="block"
+        ul.style.display ='block'
+        isOpened =true
+    
+})
+
+
+
+
+menuIcon[1].addEventListener('click',() => {
+    if(isOpened == true){
+
+        document.querySelector("nav").style.left = "-30%"
+        document.querySelector(".darker").style.display ="none"
+        ul.style.display ='none'
+        isOpened =false
+    } 
+})
