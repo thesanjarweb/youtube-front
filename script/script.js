dda6b9927c7745b9842d0544153f12c913e04a60
thesanjar09 <jumnazarjumnazar614@gmail.com>
2024-06-05 14:39:49 +0300

    Routing

--- script/script.js
+++ script/script.js
@@ -30,14 +30,80 @@ menuIcon[1].addEventListener('click',() => {
 })
 
 
+let page ="home"
+
 let lis = document.querySelectorAll("li")
 lis.forEach(li => {
 
     li.onclick = function(){
+        console.log(li.id)
+        page = li.id
 
-    lis.forEach(li =>{
-lis.classList.remove("active")
-    })
+
+       switch(page){
+        case "Home":{
+            document.querySelector("#root").innerHTML =
+            `<h1>Bu home</h1>`
+            
+
+           
+        
+
+            break;
+        }
+        case "Shorts":{
+            document.querySelector("#root").innerHTML =
+            '<h1>Bu Shorts</>';
+            break;
+        }
+        case "Subscriptions":{
+            document.querySelector("#root").innerHTML =
+            '<h1>Bu Subscriptions</>';
+            break;
+        }
+        case "History":{
+            document.querySelector("#root").innerHTML =
+            '<h1>Bu History</>';
+            break;
+        }
+        case "Playlists":{
+            document.querySelector("#root").innerHTML =
+            '<h1>Bu Playlists</>';
+            break;
+        }
+   
+               
+        case "Watch later":{
+            document.querySelector("#root").innerHTML =
+            '<h1>Bu Watch later</>';
+            break;
+        }
+        case "Liked-videos":{
+            document.querySelector("#root").innerHTML =
+            '<h1>Bu Liked-videos</>';
+            break;
+        }
+        default: {
+            document.querySelector("#root").innerHTML =
+                '<h1>404 not found '
+            
+        }
+       }
+
+    
+
+        lis.forEach(li =>{
+            li.classList.remove("active")
+        })
+
+
         li.classList.add("active")
+
+        console.log(li.id)
+
+      
     }
+
+
+  
 })
